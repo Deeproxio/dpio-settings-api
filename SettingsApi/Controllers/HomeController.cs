4040a9a3ace912bf0760151a2e8cb1969ecfb7fd
Serge Korolchuk <sergey.korolchuk.dev@gmail.com>
2018-09-22 04:31:49 +0300

    App svc renamed to Settings Api + moved from Ai Conception platform

---
+++ SettingsApi/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Deeproxio.SettingsApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deeproxio.SettingsApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+	    public async Task<StringContent> SampleLogin()
+	    {
+		    var client = new HttpClient();
+
+		    var stringTask = await client.GetAsync("http://aiconception.accountsvc/Account/Login");
+
+		    return new StringContent(stringTask.ToString());
+	    }
+	}
+}
---
+++ SettingsApi/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Deeproxio.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Deeproxio.SettingsApi
+{
+    public class Startup : IServerStartup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+			var builder = new ConfigurationBuilder()
+		        .SetBasePath(env.ContentRootPath)
+		        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+		        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+	        builder.AddEnvironmentVariables();
+	        Configuration = builder.Build();
+		}
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+	        services.AddDistributedRedisCache(options =>
+	        {
+		        options.Configuration = "redis-local";
+		        options.InstanceName = "master";
+	        });
+		}
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
